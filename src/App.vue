4dc778c3dfc1b9d06062a86403f1add335947ce5
Eduardo Moraes <adomoraes@gmail.com>
2024-03-13 16:58:49 -0300

    Adding page routes config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
     <v-main>
+      <nav>
+        <router-link to="/">Wishlists</router-link> |
+        <router-link to="/wishlistsList">Wishlists Dash</router-link> |
+        <router-link to="/wishlistsItems">Wishlist Items</router-link>
+      </nav>
       <router-view />
     </v-main>
   </v-app>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WishlistsView from '../views/WishlistsView.vue'
 
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+const routes = [
+  {
+    path: '/',
+    name: 'wishlists',
+    component: WishlistsView
+  },
+  {
+    path: '/wishlistsList',
+    name: 'wishlistsList',
+    component: () => import(/* webpackChunkName: "wishlistsList" */ '../views/WishlistsListView.vue')
+  },
+  {
+    path: '/wishlistsItems',
+    name: 'wishlistsItems',
+    component: () => import(/* webpackChunkName: "wishlistItems" */ '../views/WishlistsItemsView.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
