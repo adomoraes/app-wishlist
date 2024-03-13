0fd11761abb75a55d639a4c0f228d111611d44ac
Eduardo Moraes <adomoraes@gmail.com>
2024-03-13 18:28:56 -0300

    Adding notify component

---
+++ src/components/WishlistsNotify.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="notifyContainer">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WishlistsNotify",
+    props: {
+        message: String
+    }
+}
+</script>
