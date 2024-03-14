6e7f2e12c8070306113d3838c0d97bd2df640dbb
Eduardo Moraes <adomoraes@gmail.com>
2024-03-14 01:25:19 -0300

    Adding nav vuetify markup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <nav>
-        <router-link to="/">Wishlists</router-link> |
-        <router-link to="/wishlistsList">Wishlists Dash</router-link> |
-        <router-link to="/wishlistsItems">Wishlist Items</router-link>
+      <nav class="mx-auto my-4" style="width: 400px;">
+        <v-chip color="primary" variant="flat" class="mr-4">
+          <router-link to="/">Nova Wishlist</router-link> 
+        </v-chip>
+        <v-chip color="purple" variant="flat" class="mr-4">
+          <router-link to="/wishlistsList">Dashboard</router-link>
+        </v-chip>
+        <v-chip color="green" variant="flat">
+          <router-link to="/wishlistsItems">Produtos</router-link>
+        </v-chip>
       </nav>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script setup>
+<script scoped>
   //
 </script>
